bd5a95c1020237a726642ba63c74cedab0982254
Vladyslav Aviedov <vladaviedov@protonmail.com>
2022-07-29 22:15:03 -0400

    old repo import

---
+++ src/main/java/org/recordrobotics/munchkin/commands/manual/ManualRotator.java
@@ -0,0 +1,48 @@
+package org.recordrobotics.munchkin.commands.manual;
+
+import org.recordrobotics.munchkin.control.IControlInput;
+import org.recordrobotics.munchkin.subsystems.Rotator;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/**
+ * Rotator teleop command
+ */
+public class ManualRotator extends CommandBase {
+	private Rotator _rotator;
+	private IControlInput _controls;
+
+	private static final double MIN_SPEED = 0.15;
+
+	public ManualRotator(Rotator rotator, IControlInput control) {
+		if (rotator == null) {
+			throw new IllegalArgumentException("Rotator is null");
+		}
+		if (control == null) {
+			throw new IllegalArgumentException("Control is null");
+		}
+
+		_rotator = rotator;
+		_controls = control;
+		addRequirements(_rotator);
+	}
+
+	@Override
+	public void initialize() {
+		_rotator.rotate(0);
+	}
+
+	@Override
+	public void execute() {
+		double speed = _controls.getRotate();
+		if (Math.abs(speed) < MIN_SPEED) {
+			speed = 0;
+		}
+		_rotator.rotate(speed);
+	}
+
+	@Override
+	public void end(boolean isInterrupted) {
+		_rotator.rotate(0);
+	}
+}
